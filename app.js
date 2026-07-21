4dbfceb6fe41bc6a08f1d80644f198861d7738ed
DDRNFinch <dfinch1984@hotmail.co.uk>
2026-07-21 23:16:52 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,9 +1,1 @@
-const assignments=[
-'Assignment 1','Assignment 2','Assignment 3','Assignment 4','Assignment 5'
-];
-const ul=document.getElementById('assignments');
-assignments.forEach(a=>{
- const li=document.createElement('li');
- li.textContent=a+' - Not Started';
- ul.appendChild(li);
-});
+const docs=['Assignment_01.pdf','Assignment_02.pdf'];const ul=document.getElementById('docs');docs.forEach(d=>{const li=document.createElement('li');li.textContent=d;ul.appendChild(li);});
