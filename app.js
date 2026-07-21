029798f51efe30a5c7d3f3e44aa32668ceb38991
DDRNFinch <dfinch1984@hotmail.co.uk>
2026-07-21 23:16:22 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-document.querySelector('h1').insertAdjacentHTML('afterend','<p>Academy module foundation loaded.</p>');
+const assignments=[
+'Assignment 1','Assignment 2','Assignment 3','Assignment 4','Assignment 5'
+];
+const ul=document.getElementById('assignments');
+assignments.forEach(a=>{
+ const li=document.createElement('li');
+ li.textContent=a+' - Not Started';
+ ul.appendChild(li);
+});
